f732f89c7e98477bc8b46d79a58109d21de69c80
Mariana Rionda <maririonda@gmail.com>
2022-08-12 12:28:36 -0300

    Proyects description

--- src/components/CardProjects/CardProject.jsx
+++ src/components/CardProjects/CardProject.jsx
@@ -4,33 +4,24 @@ import StylesCard from "./CardPublication.module.scss";
 
 
 const CardProject = ({ id, summary, album, title }) => {
-
-return (
+  return (
     <div className={StylesCard.comp_card_publication}>
-        <div className={StylesCard.card}>
-
-
-
-            <div className={StylesCard.cardimage}>
-
-            <div>
-              <Carousel fade>
-                {album?.map((e,i) => {
-                return <Carousel.Item key={i}>
-                <img src={e} alt="First slide"
-              />
-            </Carousel.Item>
-        })}
-
-    </Carousel>
-    
+      <div className={StylesCard.card}>
+        <div className={StylesCard.cardimage}>
+          <div>
+            <Carousel fade>
+              {album?.map((e,i) => {
+              return  <Carousel.Item key={i}>
+                        <img src={e} alt="First slide"/>
+                      </Carousel.Item>
+              })}
+            </Carousel>
           </div>
-          </div>
-
+        </div>
+        <div className={StylesCard.cardtext}>
+          <p className={StylesCard.title}>{title}</p>
           <div className={StylesCard.cardtext}>
-            <p className={StylesCard.title}>{title}</p>
-            <div className={StylesCard.cardtext}>
-            <p className={StylesCard.title}>{summary}</p>
+          <p className={StylesCard.title}>{summary}</p>
             </div>
 
         </div>
--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -1,10 +1,10 @@
-import React from "react";
+import React, { useState } from "react";
 
 import NavBar from "../NavBar/NavBar"
 import styleProjects from "./StyleProjects.module.scss"
 
 import CardProject from "../CardProjects/CardProject";
-import { useState } from "react";
+
 import { PaginationLeft } from "../Pagination/Pagination";
 import {PaginationRight} from "../Pagination/Pagination";
 const {Projects} = require("./Data.json")
@@ -15,6 +15,13 @@ export default function Proyects(){
     const [ProjectsPerPage, setProjectsPerPage] = useState(1);
     const indexOfLastProjects = CurrentPage * ProjectsPerPage;
     const indexOfFirstProjects = indexOfLastProjects - ProjectsPerPage;
+    const [lenguage,setLenguage] = useState(1)
+  
+    function len(){
+    if(lenguage===1)setLenguage(0)
+    if(lenguage===0)setLenguage(1)
+    }
+    
     let currentProjects
     if (Projects.length > 0) {
     currentProjects = Projects?.slice(
@@ -30,40 +37,54 @@ export default function Proyects(){
                     <NavBar/>
                 </div>
                 <div className={styleProjects.divPaginationLeft}>
-                        <PaginationLeft className={styleProjects.paginationleft}
-                            ProjectsPerPage={ProjectsPerPage}
-                            Projects={Projects.length}
-                            pagination = {pagination}
-                            CurrentPage = {CurrentPage}
-                        />
+                    <PaginationLeft className={styleProjects.paginationleft}
+                        ProjectsPerPage={ProjectsPerPage}
+                        Projects={Projects.length}
+                        pagination = {pagination}
+                        CurrentPage = {CurrentPage}
+                    />
                 </div>
                 <div className={styleProjects.divHome}>
-
-
-                {currentProjects.map((e, i) => {
-                    return (
-                    <div className={styleProjects.carousel}>
-                        <CardProject
-                            key={i}
-                            id={e.id}
-                            album={e.album}
-                            title={e.title}
-                            summary={e.summary}
-                        />
+                    <div className={styleProjects.switchButton} >
+                        <p>English/Spanish</p>
+                        <input type="checkbox"  id="switch-label" onChange={()=> len()} className={styleProjects.switchbuttoncheckbox} />
+                        <label for="switch-label" className={styleProjects.switchButtonLabel} ></label>
                     </div>
-                );
-                })}
-                
+                    {currentProjects.map((e, i) => {
+                        return (
+                        <div className={styleProjects.carousel}>
+                            {lenguage===0
+                                ?   <CardProject
+                                        key={i}
+                                        id={e.id}
+                                        album={e.album}
+                                        title={e.title}
+                                        summary={e.summarySP}
+                                        deploy={e.deploy}
+                                        video={e.video}
+                                    />
+                                :   <CardProject
+                                        key={i}
+                                        id={e.id}
+                                        album={e.album}
+                                        title={e.title}
+                                        summary={e.summaryEN}
+                                        deploy={e.deploy}
+                                        video={e.video}
+                                    />
+                            }
+                        </div>
+                    );
+                    })}
                 </div>
                 <div className={styleProjects.divPaginationRight}>
-                        <PaginationRight className ="pagination"
-                            ProjectsPerPage={ProjectsPerPage}
-                            Projects={Projects.length}
-                            pagination = {pagination}
-                            CurrentPage = {CurrentPage}
-                        />
+                    <PaginationRight className ="pagination"
+                        ProjectsPerPage={ProjectsPerPage}
+                        Projects={Projects.length}
+                        pagination = {pagination}
+                        CurrentPage = {CurrentPage}
+                    />
                 </div>
-        
             </div>
         
     )
