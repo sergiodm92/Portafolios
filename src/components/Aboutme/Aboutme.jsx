73a1f79c33190e0b1ebf759cc63905686bff32ca
Sergio Andrés David Maioli <97261829+sergiodm92@users.noreply.github.com>
2022-12-30 16:11:08 -0300

    Update Aboutme.jsx

--- src/components/Aboutme/Aboutme.jsx
+++ src/components/Aboutme/Aboutme.jsx
@@ -15,11 +15,12 @@ export default function Aboutme(){
                         <h1 className={styleAboutme.tit}>Sobre mi:</h1>
                     </div>
                     <div className={styleAboutme.parrafo}>
-                        <p className={styleAboutme.parr}>Hola soy Full Stack Web Developer me dedico al desarrollo de paginas web tanto como en Front-End como Back-End.<br/>
-                            Soy una persona apasionada por el codigo, los algoritmos y el diseño, siempre buscando mejorar y dispuesto a aprender.<br/>
-                            Me gustan los desafios y siempre busco alcanzar nuevos objetivos.<br/>
-                            Aunque me forme como Ingeniero Civil siempre me apasionó la programación y es por ello que es a lo unico que me dedico ahora! 
-                            Actualmente me encuentro desarrollando proyectos de manera freelance, y capacitandome continuamente.</p>
+                        <p className={styleAboutme.parr}>Hola soy Full Stack Web Developer me dedico al desarrollo web tanto en Front-End como Back-End.<br/>
+                            Me apasionada el codigo, los algoritmos, la resolución de problemas, pero también el diseño UI/UX, busco mejorar mi código haciendoló mas limpio y eficiente, disfruto aprender nuevas tecnologías y metodologias.<br/>
+                            Me gustan los desafios, el trabajo en equipo y siempre busco alcanzar nuevos objetivos.<br/>
+                            Aunque me forme como Ingeniero Civil en la universidad, siempre me apasionó la programación y es por ello que me dedico desde 2020 a esto.  
+                            Actualmente me encuentro desarrollando proyectos de manera freelance con un pequeño equipo de trabajo, y capacitandome continuamente.
+                            Estoy en busqueda de una empresa donde pueda crecer de manera profesional e introducirme en el mundo IT</p>
                     </div>
                 </div>
 
